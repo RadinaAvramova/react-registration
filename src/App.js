71a8236f61b4bd56c8cbdbf8c5def61b5b1e0b8a
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-03-15 15:16:38 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import HomePage from "./Components/HomePage";
-import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
-// ..
-AOS.init({
-  duration: 1000,
-});
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+import Header from './components/shared/Header/header.component';
+import Footer from './components/shared/Footer/footer.component';
+import Register from './components/Register/register.component';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Header />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={4}>
+          <Register />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Footer />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
